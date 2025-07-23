c36e79729acc2a56962d9bd04af2d3169e92ca18
ahmedragab15 <ragabahmed154@gmail.com>
2025-07-23 06:32:01 +0300

    getUserFromToken on header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Input } from "../ui/input";
@@ -5,8 +6,14 @@ import { Bell, Menu, Search, ShoppingCart, UserRound } from "lucide-react";
 import { Button } from "../ui/button";
 import ActiveLink from "../shared/ActiveLink";
 import { images } from "../shared/Images";
+import { userLogoutAction } from "@/actions/userActions";
+import { redirect } from "next/navigation";
 
-const Header = () => {
+const Header = ({ user }: { user: JwtPayload | null }) => {
+  const handleLogout = async () => {
+    await userLogoutAction();
+    redirect("/login");
+  };
   return (
     <header className="flex items-center justify-between lg:justify-evenly lg:gap-8 py-4 px-4 lg:px-10 bg-white border shadow rounded-4xl my-8 max-w-10/12 xl:max-w-8/12 mx-auto">
       <div>
@@ -18,7 +25,7 @@ const Header = () => {
         <ul className="flex items-center gap-6 px-6">
           <li className="relative">
             <ActiveLink
-              href="/"
+              href={"/home"}
               className="font-medium duration-200 hover:text-primary"
               activeClassName="text-primary after:absolute after:-bottom-3 after:left-1/2 after:w-2 after:h-2 after:bg-primary after:rounded-full"
             >
@@ -43,15 +50,6 @@ const Header = () => {
               Courses
             </ActiveLink>
           </li>
-          <li className="relative">
-            <ActiveLink
-              href="#"
-              className="font-medium duration-200 hover:text-primary"
-              activeClassName="text-primary after:absolute after:-bottom-3 after:left-1/2 after:w-2 after:h-2 after:bg-primary after:rounded-full"
-            >
-              Tech With Us
-            </ActiveLink>
-          </li>
         </ul>
       </nav>
       <div className="flex items-center relative lg:border-1 border-gray-300 rounded-2xl lg:flex-1 ml-auto">
@@ -65,15 +63,30 @@ const Header = () => {
         </Button>
       </div>
       <div className="flex items-center gap-1 md:gap-4">
-        <Link href={"/cart"}>
-          <ShoppingCart className="text-primary  p-1 hover:bg-primary hover:text-white rounded-full" size={30} />
+        <Link href={"/cart"} className="relative">
+          <ShoppingCart className="text-primary p-1 hover:bg-primary hover:text-white rounded-full" size={30} />
+          <span className="absolute top-0 right-0 text-xs bg-primary text-white w-4 h-4 flex items-center justify-center rounded-full pointer-events-none">3</span>
         </Link>
-        <Bell className="text-primary hover:bg-primary  p-1 hover:text-white rounded-full" size={30} />
-        <Link href={"/account"}>
-          <UserRound className="text-primary hover:bg-primary  p-1 hover:text-white rounded-full" size={30} />
-        </Link>
+        {user && (
+          <>
+            <Bell className="text-primary hover:bg-primary p-1 hover:text-white rounded-full" size={30} />
+            <Link href={"/account"} className="flex items-center text-primary hover:bg-primary py-1 px-2 hover:text-white rounded-full">
+              <UserRound size={30} />
+              <span>{user.fullName.split(" ")[0]}</span>
+            </Link>
+          </>
+        )}
+        <h3>
+          {user ? (
+            <Button onClick={handleLogout}>Logout</Button>
+          ) : (
+            <ActiveLink href="/register" className="font-medium duration-200 hover:text-primary" activeClassName="text-primary">
+              Sign In
+            </ActiveLink>
+          )}
+        </h3>
       </div>
-      <Menu className="md:hidden text-primary hover:bg-primary  p-1 hover:text-white rounded-full" size={30} />
+      <Menu className="md:hidden text-primary hover:bg-primary p-1 hover:text-white rounded-full" size={30} />
     </header>
   );
 };
