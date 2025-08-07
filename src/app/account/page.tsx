50575fc837ef37d13b564895f4068aa5ff858f86
ahmedragab15 <ragabahmed154@gmail.com>
2025-08-07 07:56:42 +0300

    Enrollments functionality

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,23 +1,24 @@
+import { getEnrollmentsAction } from "@/actions/enrollmentsActions";
 import { Courses, Heading } from "@/components";
 import { Button } from "@/components/ui/button";
-import { user } from "@/dummyData";
+import { getUserFromToken } from "@/lib/verifyJWT";
 import React from "react";
 
-const MyClass = () => {
+const MyClass = async () => {
+  const user = await getUserFromToken();
+  if (!user) return null;
+  const enrollments = await getEnrollmentsAction(user.id);
+
   return (
-      <div className="space-y-6">
-        <Heading title="My Class" description="Continue to learn with new knowledge" />
-        <div className="flex gap-4">
-          <Button>All</Button>
-          <Button variant={"outline"}>
-            Started
-          </Button>
-          <Button variant={"outline"}>
-            Finished
-          </Button>
-        </div>
-            <Courses progressCourses={user.allCourses} className="justify-center lg:justify-start" />
+    <div className="space-y-6">
+      <Heading title="My Class" description="Continue to learn with new knowledge" />
+      <div className="flex gap-4">
+        <Button>All</Button>
+        <Button variant={"outline"}>Started</Button>
+        <Button variant={"outline"}>Finished</Button>
       </div>
+      <Courses progressCourses={enrollments} className="justify-center lg:justify-start" />
+    </div>
   );
 };
 
--- src/components/cards/ProgressCourseCard.tsx
+++ src/components/cards/ProgressCourseCard.tsx
@@ -4,25 +4,31 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import RatingProgress from "../RatingProgress";
+import { Prisma } from "@/generated/prisma/client";
+
+type Enrollments = Prisma.EnrollmentGetPayload<{
+  include: { user: true; course: { include: { category: true } } };
+}>;
+
 type CourseCardProps = {
-  course: IProgressCourse;
+  course: Enrollments;
 };
 
 const ProgressCourseCard = ({ course }: CourseCardProps) => {
   return (
-    <Link href={`/all-courses/${course.slug}?id=${course.id}`}>
-      <div className="flex flex-col bg-slate-100 w-64 max-w-full pb-6 gap-4 rounded-md shadow hover:shadow-xl duration-200">
-        <Image src={course.image} alt="course image" width={400} height={400} className="rounded-md max-w-full" />
+    <Link href={`/all-courses/${course.course.slug}?id=${course.id}`}>
+      <div className="flex flex-col bg-slate-100 dark:bg-slate-700 w-64 max-w-full pb-6 gap-4 rounded-md shadow hover:shadow-xl duration-200">
+        <Image src={course.course.imageUrl} alt="course image" width={400} height={400} className="rounded-md max-w-full" />
         <div className="space-y-2 px-4">
           <Badge variant="outline" className="text-xs text-primary relative border-0 -ml-2">
             <span className="h-2 w-2 rounded-full bg-primary"></span>
-            <span>{course.category}</span>
+            <span>{course.course.category.title}</span>
           </Badge>
-          <h3 className="font-medium">{course.title}</h3>
+          <h3 className="font-medium line-clamp-1">{course.course.title}</h3>
           <div>
-            <RatingProgress count={course.allLessons > 0 ? (course.finishedLessons / course.allLessons) * 100 : 0} className="h-2" />
-            <span className="text-gray-600 text-xs">
-              {course.finishedLessons}/{course.allLessons} Lessons
+            <RatingProgress count={course.progress > 0 ? (course.finishedLessons / course.progress) * 100 : 0} className="h-2" />
+            <span className="text-muted-foreground text-xs">
+              {course.finishedLessons}/{course.progress} Lessons
             </span>
           </div>
           <Button variant={"outline"}>Continue</Button>
